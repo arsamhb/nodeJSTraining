0868e28355a80b9dfcca969647c482ad1e415ee4
Rsam <arsam.hb@gmail.com>
2023-09-09 13:36:24 +0330

    getting the meaning of async in working with files

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
-const {add} = require("./math")
+const fsPromises = require("fs").promises;
+const path = require("path");
 
-console.log(add(2,2));
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "utf8"
+    );
+    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
+    console.log(data);
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "newPromisfile.txt"),
+      "\n\nbacktogame"
+    );
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "newPromisfile.txt"),
+      data
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "files", "newPromisfile.txt"),
+      path.join(__dirname, "files", "renamed.txt")
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fileOps();
+
+process.on("uncaughtException", (err) => {
+  console.log(`there was an error a uncaught ${err}`);
+  process.exit(1);
+});
